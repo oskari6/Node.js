beceac682d2eb84b6c5476b50aafe0c0c94de962
oskari6 <oskarisulkakoski@gmail.com>
2024-02-29 10:34:57 +0200

    10

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
 
