64d83a1efe5df6f45ec1fb5287a50fe40228abf6
oskari6 <oskarisulkakoski@gmail.com>
2024-05-05 15:01:29 +0300

    sefsdf

---
+++ aws2/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("test");
+});
+
